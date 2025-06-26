8aa11f6872b596d9ea682c466d14c037127035ba
Kristen Swaim <kswaimdev@gmail.com>
2025-06-26 10:31:56 -0400

    fixed keystatic error resulting from accidental changes to auctionwidget

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,19 +1,19 @@
-// keystatic.config.ts
 import { config, fields, collection } from '@keystatic/core';
-
+import { github } from '@keystatic/core/storage/github';
 
 export default config({
   ui: {
-    brand: { name: 'Ron Mckee Auction Service',
+    brand: {
+      name: 'Ron McKee Auction Service',
     },
   },
-  storage: {
-    kind: 'github',
+  storage: github({
     repo: {
       owner: 'kdswaim',
-      name: 'mckee-auction'
-    }
-  },
+      name: 'mckee-auction',
+    },
+    branch: 'main', // or your default branch
+  }),
   collections: {
     posts: collection({
       label: 'Auctions',
@@ -23,11 +23,12 @@ export default config({
       schema: {
         title: fields.slug({ name: { label: 'Title' } }),
         date: fields.date({ label: 'Auction Date' }),
-        time: fields.text({
-            label: 'Auction Time',
-          }),
+        time: fields.text({ label: 'Auction Time' }),
         location: fields.text({ label: 'Location' }),
-        image: fields.url({ label: 'Image URL', description: '(Optional) Upload your images to your portal (http://www.ronmckeeauctionservice.com/upload) and then paste the URL!' }),
+        image: fields.url({
+          label: 'Image URL',
+          description: '(Optional) Upload your images to your portal (http://www.ronmckeeauctionservice.com/upload) and then paste the URL!',
+        }),
         link: fields.url({ label: 'Link to full details', description: 'Optional link' }),
         content: fields.markdoc({ label: 'Content' }),
       },
